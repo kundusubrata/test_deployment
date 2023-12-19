ce005740e31a699c8b02dfe4eb3aaaffadb8d668
Subrata Kundu <kundu.subrata2020@outlook.com>
2023-12-19 17:09:47 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+dotenv.config({
+    path: "./config.env",
+});
+
+const port = process.env.PORT || 3000;
+
+const home = fs.readFileSync("./home.html","utf-8");
+
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.url);
+    if(req.url==="/"){
+        return res.end(home);
+    }
+    else{
+        return res.end("<h1>404 Page not found</h1>");
+    }
+});
+
+server.listen(port,()=>{
+    console.log(`Server is working.`);
+});
